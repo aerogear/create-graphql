a25208535902f58a2004022acd425883ccc0c939
Sibelius Seraphini <sibeliusseraphini@gmail.com>
2016-12-21 13:03:20 -0200

    add mutation with schema tests

--- packages/generator/src/mutation/__tests__/MutationGenerator.spec.js
+++ packages/generator/src/mutation/__tests__/MutationGenerator.spec.js
@@ -1,15 +1,21 @@
 import helper from 'yeoman-test';
 import assert from 'yeoman-assert';
 import path from 'path';
+import fs from 'fs-extra';
 
-import { getFileContent } from '../../../test/helpers';
+import {
+  getFileContent,
+  getFixturePath,
+} from '../../../test/helpers';
 
 import { getConfigDir } from '../../utils';
 
-const mutationGenerator = helper.run(path.join(__dirname, '..'));
+const mutationGenerator = path.join(__dirname, '..');
 
 it('generate mutation files', async () => {
-  const folder = await mutationGenerator.withArguments('Example').toPromise();
+  const folder = await helper.run(mutationGenerator)
+    .withArguments('Example')
+    .toPromise();
 
   const destinationDir = getConfigDir('mutation');
   const destinationTestDir = getConfigDir('mutation_test');
@@ -23,6 +29,34 @@ it('generate mutation files', async () => {
     edit: getFileContent(`${folder}/${destinationDir}/ExampleEditMutation.js`),
     addTest: getFileContent(`${folder}/${destinationTestDir}/ExampleAddMutation.spec.js`),
     editTest: getFileContent(`${folder}/${destinationTestDir}/ExampleEditMutation.spec.js`),
+  };
+
+  expect(files).toMatchSnapshot();
+});
+
+it('generate mutation files with Schema', async () => {
+  const folder = await helper.run(mutationGenerator)
+    .inTmpDir(dir =>
+      fs.copySync(
+        getFixturePath('Post'),
+        path.join(dir, 'src/model/Post.js'),
+      ),
+    )
+    .withArguments('Post Post')
+    .toPromise();
+
+  const destinationDir = getConfigDir('mutation');
+  const destinationTestDir = getConfigDir('mutation_test');
+
+  assert.file([
+    `${destinationDir}/PostAddMutation.js`, `${destinationDir}/PostEditMutation.js`,
+  ]);
+
+  const files = {
+    add: getFileContent(`${folder}/${destinationDir}/PostAddMutation.js`),
+    edit: getFileContent(`${folder}/${destinationDir}/PostEditMutation.js`),
+    addTest: getFileContent(`${folder}/${destinationTestDir}/PostAddMutation.spec.js`),
+    editTest: getFileContent(`${folder}/${destinationTestDir}/PostEditMutation.spec.js`),
   };
 
   expect(files).toMatchSnapshot();
