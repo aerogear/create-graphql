8e48ed7ec4c668ef29c6f0319427739a3948d1ee
Sibelius Seraphini <sibeliusseraphini@gmail.com>
2017-01-04 11:06:08 -0200

    Templates for Relay frontend
    Add.js.template - simple screen with a form with all input fields
    of the mutation from schema.json, it commit mutation onSubmit
    
    AddMutation.js.template - simple add mutation template
    
    Edit.js.template - simple screen with a form to edit a graphql object type
    
    EditMutation.js.template - simple edit mutation template
    
    List.js.template - list of objects from relay
    
    View.js.template - tabbed detail view of graphql object type
    
    WIP of #12

---
+++ packages/generator/src/frontend/list/index.js
@@ -0,0 +1,62 @@
+import Generator from 'yeoman-generator';
+import pluralize from 'pluralize';
+import {
+  getMongooseModelSchema,
+  getConfigDir,
+  getRelativeConfigDir,
+  camelCaseText,
+  uppercaseFirstLetter,
+} from '../../utils';
+
+class ListGenerator extends Generator {
+  constructor(args, options) {
+    super(args, options);
+
+    this.argument('name', {
+      type: String,
+      required: true,
+    });
+
+    // TODO read schema.json
+
+    this.destinationDir = getConfigDir('list');
+  }
+
+  _getConfigDirectories() {
+    return getRelativeConfigDir('loader', ['model', 'connection']);
+  }
+
+  generateList() {
+    // const schema = this.options.model ?
+    //   getMongooseModelSchema(this.options.model, true)
+    //   : null;
+
+    const name = uppercaseFirstLetter(this.options.name);
+
+    const templatePath = this.templatePath('List.js.template');
+
+    // const templatePath = schema ?
+    //   this.templatePath('LoaderWithSchema.js.template')
+    //   : this.templatePath('Loader.js.template');
+    //
+    // const directories = this._getConfigDirectories();
+
+    const pluralName = pluralize(this.options.name);
+
+    const destinationPath = this.destinationPath(`${this.destinationDir}/${name}List.js`);
+    const templateVars = {
+      name,
+      rawName: this.options.name,
+      pluralName,
+      pluralCamelCaseName: camelCaseText(pluralName),
+    };
+
+    this.fs.copyTpl(templatePath, destinationPath, templateVars);
+  }
+
+  end() {
+    this.log('🔥 List created!');
+  }
+}
+
+module.exports = ListGenerator;
